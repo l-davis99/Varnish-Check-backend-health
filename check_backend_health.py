ddf14c24f8bce47c864b4cc5d30a6ff06706b8c5
Luke <luke.davis99@pm.me>
2022-05-14 17:41:03 +0100

    added in-line comments

--- check_backend_health.py
+++ check_backend_health.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
+#
+# Author: Luke Davis (luked@positive-internet.com)
+# Date: 2022-05-14
+# Description: Script to check varnish backends
+#
 
 import subprocess
 import argparse
 
+# define nrpe agent exit status
 UNKNOWN = -1
 OK = 0
 WARNING = 1
 CRITIAL = 2
 
+# handle cli-based parameters
 parser = argparse.ArgumentParser(prog="check_varnish_health", description='Check the health of each Varnish backend')
-
-# Add the arguments
 parser.add_argument('--verbose', metavar='verbose', type=bool, default=False, help='Enable verbose output')
 parser.add_argument('path', metavar='path', type=str, help='the path to list')
 parser.add_argument('args', metavar='args', type=str, help='list backend parameter')
@@ -18,17 +23,27 @@ parser.add_argument('args', metavar='args', type=str, help='list backend paramet
 # Execute the parse_args() method
 args = parser.parse_args()
 
+# assign arguments to variables
 verbose = args.verbose
 varnishadm_path = args.path
 varnishadm_args = args.args
 
+# run varnishadm command based on above variables and parameters
 command = subprocess.run([varnishadm_path, varnishadm_args], capture_output=True, text=True).stdout
 
+# split output of varnishadm command 
 cmd = command.split("\n")
 
+# define arrays for backend names depending on status of for loop
 b_healthy, b_sick, b_unknown = [], [], []
+
+# loop over each line from varnishadm output
 for l in cmd:
+
+    # only get the lines we're interested in.
     if l.startswith("reload") or l.startswith("boot"):
+
+        # depending on what the line contains, place it in the correct array
         if "healthy" in l:
             p = l.partition('.')[2]
             b_healthy.append(p.split(" ")[0])
@@ -36,13 +51,16 @@ for l in cmd:
             p = l.partition('.')[2]
             b_sick.append(p.split(" ")[0])
 
+# verbose ouput to see what the script is detectign
 if verbose: print(f"Healthy Backends: {b_healthy}")
 if verbose: print(f"Sick Backends: {b_sick}")
 
 if b_sick:
     print("%s backends are sick! %s" %  (len(b_sick), " ".join(b_sick)))
+    exit(CRITICAL)
 
 if b_healthy:
     print ("All %s backends are healthy" % (len(b_healthy)))
+    exit(OK)
 
 
