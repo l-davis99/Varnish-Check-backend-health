bed64e677482e8fff20f4f6d3c31038ea5f5b737
Luke <luke.davis99@pm.me>
2022-05-15 16:06:37 +0100

    message changes

--- check_backend_health.py
+++ check_backend_health.py
@@ -16,7 +16,7 @@ CRITIAL = 2
 
 # handle cli-based parameters
 parser = argparse.ArgumentParser(prog="check_varnish_health", description='Check the health of each Varnish backend')
-parser.add_argument('--verbose', metavar='verbose', type=bool, default=False, help='Enable verbose output')
+parser.add_argument('--verbose', type=bool, default=False, help='Enable verbose output')
 parser.add_argument('path', metavar='path', type=str, help='the path to list')
 parser.add_argument('args', metavar='args', type=str, help='list backend parameter')
 
@@ -53,14 +53,16 @@ for l in cmd:
 
 # verbose ouput to see what the script is detectign
 if verbose: print(f"Healthy Backends: {b_healthy}")
-if verbose: print(f"Sick Backends: {b_sick}")
+if verbose: print(f"Sick Backends: {b_sick}\n")
 
 if b_sick:
-    print("%s backends are sick! %s" %  (len(b_sick), " ".join(b_sick)))
+    print("CRITICAL: %s backends are sick! %s" %  (len(b_sick), " ".join(b_sick)))
     exit(CRITICAL)
 
+if not b_sick and not b_healthy:
+    print("UNKNOWN: No backends found (check verbose output)")
+    exit(UNKNOWN)
+
 if b_healthy:
-    print ("All %s backends are healthy" % (len(b_healthy)))
+    print ("OK: All %s backends are healthy" % (len(b_healthy)))
     exit(OK)
-
-
